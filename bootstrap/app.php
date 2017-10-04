6c6a2641e1cde91853d1fb337085084bf1eab59a
Ben Bagley <ben@benbagley.co.uk>
2017-10-04 02:08:19 +0100

    added redis predis

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,12 @@ $app = new Slim\App([
     ],
 ]);
 
+$redis = new Predis\Client(array(
+    'host' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
+    'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
+    'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
+));
+
 $container = $app->getContainer();
 
 $container['view'] = function ($container) {
