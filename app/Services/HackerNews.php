edffde67190cfb1885bc99757b46829c56ec7e53
Ben Bagley <ben@benbagley.co.uk>
2017-10-07 21:41:37 +0100

    changes beststories to newstories

--- app/Services/HackerNews.php
+++ app/Services/HackerNews.php
@@ -6,7 +6,7 @@ class HackerNews extends ServiceAbstract
 {
   public function get($limit = 20)
   {
-    $response = $this->client->request('GET', 'https://hacker-news.firebaseio.com/v0/beststories.json');
+    $response = $this->client->request('GET', 'https://hacker-news.firebaseio.com/v0/newstories.json');
     $ids = array_slice(json_decode($response->getBody()), 0, $limit);
 
     $stories = [];
